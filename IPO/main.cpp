746a448762aaffde898535779c37078f706390ad
peru emanuel <peruemanuel1@gmail.com>
2023-08-31 22:19:00 +0300

    Initial Commit

---
+++ IPO/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  IPO
+//
+//  Created by El padrino on 31/08/2023.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
